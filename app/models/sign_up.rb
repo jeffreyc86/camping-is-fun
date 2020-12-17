b443d04cdd918369ba55e96fe9bfc1e134cf9a0d
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-12-17 16:27:20 -0500

    added validations

--- app/models/sign_up.rb
+++ app/models/sign_up.rb
@@ -4,5 +4,7 @@ class SignUp < ApplicationRecord
     belongs_to :activity
 
     validates :time, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23}
-    validates :camper_id, uniqueness: { scope: :time, message: " is already signed up for an activity at that time."}
+    validates :camper_id, uniqueness: { scope: :time, message: " is unavailable at that time."}
+    validates :camper_id, uniqueness: { scope: :activity_id, " is already signed up for that activity."}
+
 end
